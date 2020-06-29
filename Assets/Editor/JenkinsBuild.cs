b94ddc3668b82634eab9e1c9b2d222da96c251c3
jbalestrierismall <julien.balestrieri@small-studio.io>
2020-06-29 11:24:34 +0200

    add build script

---
+++ Assets/Editor/JenkinsBuild.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using System;
+
+class JenkinsBuild
+{
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "YourProject";
+    static string TARGET_DIR = "target";
+
+    [MenuItem("Custom/CI/Build Mac OS X")]
+    static void PerformMacOSXBuild()
+    {
+        string targetDir = APP_NAME + ".app";
+        GenericBuild(SCENES, TARGET_DIR + "/" + targetDir, BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = targetDir;
+        buildPlayerOptions.targetGroup = buildTargetGroup;
+        buildPlayerOptions.target = buildTarget;
+        buildPlayerOptions.options = buildOptions;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        if (report.summary.totalErrors != 0)
+        {
+            throw new Exception("BuildPlayer failure: " + report.summary.totalErrors + "(errors)");
+        }
+    }
+}
