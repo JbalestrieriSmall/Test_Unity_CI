8e1cfee885fc76f5c0ec238abac0ce6ad8af5317
jbalestrierismall <julien.balestrieri@small-studio.io>
2020-06-29 14:11:55 +0200

    add android config

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -26,6 +26,36 @@ class JenkinsBuild
         GenericBuild(SCENES, TARGET_DIR + "/" + targetPath, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows, BuildOptions.None);
     }
 
+    [MenuItem("Custom/CI/Build Android")]
+    static void PerformAndroidBuild()
+    {
+        string targetPath = APP_NAME + ".apk";
+        GenericBuild(SCENES, TARGET_DIR + "/" + targetPath, BuildTargetGroup.Standalone, BuildTarget.Android, BuildOptions.None);
+    }
+
+    static void PerformBuild(BuildTarget target)
+    {
+        string targetPath = "";
+
+        switch (target)
+        {
+            case BuildTarget.StandaloneOSX:
+                targetPath = APP_NAME + ".app";
+                break;
+            case BuildTarget.StandaloneWindows:
+                targetPath = APP_NAME + ".exe";
+                break;
+            case BuildTarget.Android:
+                targetPath = APP_NAME + ".apk";
+                break;
+            default:
+                throw new Exception("Build target not supported: " + target);
+                break;
+        }
+
+        GenericBuild(SCENES, TARGET_DIR + "/" + targetPath, BuildTargetGroup.Standalone, target, BuildOptions.None);
+    }
+
     private static string[] FindEnabledEditorScenes()
     {
         List<string> EditorScenes = new List<string>();
